fc4d875b4d44bdeefe3bee91500d6425d8f12941
antonio <antoninho.ferreira2015@gmail.com>
2026-04-22 17:13:09 -0300

    Criação do projeto

---
+++ src/main/java/com/manager/coopafi/CoopafiApplication.java
@@ -0,0 +1,13 @@
+package com.manager.coopafi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoopafiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoopafiApplication.class, args);
+	}
+
+}
